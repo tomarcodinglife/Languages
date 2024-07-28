37ae1861db255e0afb7b779fd3f7127b58f631ef
tomarcodinglife <tomarcodinglife@gmail.com>
2024-07-28 12:03:20 +0000

    updated

--- 01_basics/03_dataTypes.js
+++ 01_basics/03_dataTypes.js
@@ -1,11 +1,10 @@
-// number =>  0, 1, 2, 3, ..... etc 
+// number =>  0, 1, 2, 3, ..... etc [2 to the power 53]
 // bigint => 
 // string => "Sujit" or "Patna" or "email@gmail.com"
 // boolean => true/false
 // null => standalone value / empty data type
 // undefined => 
 // symbol => 
-
-//object 
+//object => 
 
 console.log(typeof "Sujit")
