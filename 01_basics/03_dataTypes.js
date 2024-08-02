9c7838da4f1969d69ad4adc0362fbda969816050
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-01 20:08:05 -0700

    updated

--- 01_basics/03_dataTypes.js
+++ 01_basics/03_dataTypes.js
@@ -1,10 +1,20 @@
-// number =>  0, 1, 2, 3, ..... etc [2 to the power 53]
-// bigint => 
-// string => "Sujit" or "Patna" or "email@gmail.com"
-// boolean => true/false
-// null => standalone value / empty data type
-// undefined => 
-// symbol => 
-//object => 
+/*
+number =>  0, 1, 2, 3, ..... etc [2 to the power 53]
+bigint => 
+string => "Sujit" or "Patna" or "email@gmail.com"
+boolean => true/false
+null => standalone value / empty data type
+undefined => 
+symbol => 
+object => 
+*/
+let fullName = "Sujit Kumar Singh";
+let rollNumber = 52856784854
+let emailId = "email@gmail.com";
+let pass = true;
+let state = null
+let district = ""
 
-console.log(typeof "Sujit")
+
+console.table([fullName, rollNumber, emailId, pass, state, district])
+console.table([typeof(fullName), typeof(rollNumber), typeof(emailId), typeof(pass), typeof(state), typeof(district)])
