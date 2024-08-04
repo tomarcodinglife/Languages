4a4e5c655d5a3f284351f85d5a5aa829a691fd9d
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-04 07:00:45 -0700

    updated

--- 01_basics/03_dataTypes.js
+++ 01_basics/03_dataTypes.js
@@ -26,6 +26,7 @@ Primitive Data Type (7 Types ) : String, Number, Boolean, Null, Undefined, Symbo
 Non-Primitive Data Type : Object, Array, Function, Date, RegExp, Map, Set
 */
 
+//Primitive Data Type
 let dtv01 = "Sujit Tomar" //String
 let dtv02 = 454 //number
 let dtv03 = false //boolean
@@ -34,6 +35,51 @@ let dtv05
 // let dtv06 = symbol (dtv02) // symbol
 // let dtv07 = symbol (dtv02) // symbol
 // let dtv08 = BigInt(5255252456655456620520500505050005005545488885651255) //big integer (Bigint)
-
 // console.table([typeof(dtv01), typeof(dtv02), typeof(dtv03), typeof(dtv04), typeof(dtv05)])
-console.log(dtv06 === dtv07)
+
+//Non Primitive Data Type
+
+const movies = ["Shaktiman", "Nagraj", "Balveer", "Nagin"] //Array
+let myobj = {
+    name : "Sujit Kumar Singh",
+    roll : 125205,
+    pass : true,
+    marks : 286,
+    fullMarks : 500,
+    marksPer : 286/5
+}
+
+console.log(myobj)
+
+
+const myFunction = function (){
+    console.log("Hello Worls")
+}
+
+console.log(myFunction)
+console.log(typeof (myFunction))
+
+console.log("------------------------Here----------------------");
+
+let userFirst = {
+    myName : "Ajit Kumar Singh",
+    email : "ajit@google.com",
+    age : 40,
+}
+
+let userSecond = {
+    myName : userFirst.myName,
+    email : userFirst.email,
+    age : userFirst.age
+}
+
+let userThree = userFirst
+
+userFirst.myName = "Sujit Kumar Singh";
+userFirst.email = "sujitkumarsingh@google.com";
+userFirst.age = 25
+
+console.log(userFirst)
+console.log(userSecond)
+console.log(userThree)
+
