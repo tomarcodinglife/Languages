2e1d0cb557ded36ba3422400edfe280415c1067b
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-04 06:02:44 -0700

    update

--- 01_basics/03_dataTypes.js
+++ 01_basics/03_dataTypes.js
@@ -18,3 +18,22 @@ let district = ""
 
 console.table([fullName, rollNumber, emailId, pass, state, district])
 console.table([typeof(fullName), typeof(rollNumber), typeof(emailId), typeof(pass), typeof(state), typeof(district)])
+
+/*
+Types of Data Type
+Two Types od Data Types : Primitive and Non-Primitive 
+Primitive Data Type (7 Types ) : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+Non-Primitive Data Type : Object, Array, Function, Date, RegExp, Map, Set
+*/
+
+let dtv01 = "Sujit Tomar" //String
+let dtv02 = 454 //number
+let dtv03 = false //boolean
+let dtv04 = null // null
+let dtv05 
+// let dtv06 = symbol (dtv02) // symbol
+// let dtv07 = symbol (dtv02) // symbol
+// let dtv08 = BigInt(5255252456655456620520500505050005005545488885651255) //big integer (Bigint)
+
+// console.table([typeof(dtv01), typeof(dtv02), typeof(dtv03), typeof(dtv04), typeof(dtv05)])
+console.log(dtv06 === dtv07)
