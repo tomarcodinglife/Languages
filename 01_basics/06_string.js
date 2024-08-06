8e093c2481016456c4229b36e46f00a3c64c668e
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-06 07:15:25 +0530

    update

---
+++ 01_basics/06_string.js
@@ -0,0 +1,6 @@
+const fullName = " Sujit Kumar Singh"
+const profession = "Software Engineer"
+
+/***********************************string interpolation******************************************/
+console.log ('my name is ${fullName} and my profession is ${profession}')
+
