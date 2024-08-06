dc3f8ce2a6f3568626b43d224df03bd941410f49
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-06 07:57:19 +0530

    updated

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -2,5 +2,10 @@ const fullName = " Sujit Kumar Singh"
 const profession = "Software Engineer"
 
 /***********************************string interpolation******************************************/
-console.log ('my name is ${fullName} and my profession is ${profession}')
+console.log (`my name is ${fullName.toUpperCase} and my profession is ${profession.toUpperCase}`) // (`) it is backtics
+console.log (`my name is ${fullName} and my profession is ${profession.toUpperCase}`) // (`) it is backtics
+
+const strFirst = "String Name"
+console.log (strFirst. charAt(2))
+
 
