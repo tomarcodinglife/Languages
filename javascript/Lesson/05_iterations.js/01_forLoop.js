633a55eb0697b80c2330fdb393d7aa41dfe2b5af
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-17 07:00:12 +0530

    updated

--- javascript/Lesson/05_iterations.js/01_forLoop.js
+++ javascript/Lesson/05_iterations.js/01_forLoop.js
@@ -62,6 +62,26 @@ for (let k = 1; k<=10; k++) {
 }
 
 //*************************************************************************************
+//break
+
+
+for (let index = 1; index < 20; index++) {
+    if(index == 5 ){
+        console.log("detected 5")
+        break
+    }
+    console.log(`value of i is ${index}`)
+}
+
+//*************************************************************************************
+//continue
 
 console.log("****************************** From  *********************************");
 
+for (let index = 1; index <= 20; index++) {
+    if(index == 5 ){
+        console.log("detected 5")
+        continue
+    }
+    console.log(`value of i is ${index}`)
+}
