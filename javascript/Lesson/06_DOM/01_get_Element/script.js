07ebcd7047e936a826caea67b6b77b4c755db82e
tomarcodinglife <tomarcodinglife@gmail.com>
2024-08-21 07:29:02 +0530

    updated

---
+++ javascript/Lesson/06_DOM/01_get_Element/script.js
@@ -0,0 +1,10 @@
+console.log("hi i am Javascript")
+
+
+// get Element by Id
+
+let title = "Sujit Kumar Singh"
+
+let headingName = document.getElementById("Heading-Name")
+headingName.innerText = title
+
